757e40a224da958ea229049117254f94b36587f0
苏杨 <suyang_5070@126.com>
2019-03-31 22:11:13 +0800

    'footer区完成'

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <router-view/>
+
+    <Footer/>
+  </div>
+</template>
+
+<script>
+  import Footer from './components/Footer/Footer'
+
+  export default {
+    components:{
+      Footer
+    }
+  }
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
